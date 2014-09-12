20b38c4244d9e628c95ff9271f199f2d9dd5c03b
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-09-12 16:08:06 -0400

    password protecting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Figaro.env.basic_username && password ==  Figaro.env.basic_password
+    end
+  end
 end
