986d8543b0904cdc2b9b2f76269d4827bc4957da
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-03-16 10:29:19 -0400

    Setting the correct branch for prod deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,6 +15,7 @@ role :db,  %w{ubuntu@arp.softwearcrm.com}
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 server 'arp.softwearcrm.com', user: 'ubuntu', roles: %w{web app}#, my_property: :my_value
+set :branch, 'master'
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
